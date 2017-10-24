34add5c515b7bd8834d3d431e0aca4ee8349db4e
Martin Zurowietz <martin@zurowietz.de>
2017-10-24 16:14:11 +0200

    Switch to use spaces instead of tabs

--- Watershed.py
+++ Watershed.py
@@ -6,118 +6,118 @@ import numpy as np
 # '90: Fifth in a Series, (1 September 1990); doi: 10.1117/12.24211;
 # http://dx.doi.org/10.1117/12.24211
 class Watershed(object):
-	MASK = -2
-	WSHD = 0
-	INIT = -1
-	INQE = -3
+   MASK = -2
+   WSHD = 0
+   INIT = -1
+   INQE = -3
 
-	def __init__(self, levels = 256):
-		self.levels = levels
+   def __init__(self, levels = 256):
+      self.levels = levels
 
-	# Neighbour (coordinates of) pixels, including the given pixel.
-	def _get_neighbors(self, height, width, pixel):
-		return np.mgrid[
-			max(0, pixel[0] - 1):min(height, pixel[0] + 2),
-			max(0, pixel[1] - 1):min(width, pixel[1] + 2)
-		].reshape(2, -1).T
+   # Neighbour (coordinates of) pixels, including the given pixel.
+   def _get_neighbors(self, height, width, pixel):
+      return np.mgrid[
+         max(0, pixel[0] - 1):min(height, pixel[0] + 2),
+         max(0, pixel[1] - 1):min(width, pixel[1] + 2)
+      ].reshape(2, -1).T
 
-	def apply(self, image):
-		current_label = 0
-		flag = False
-		fifo = []
+   def apply(self, image):
+      current_label = 0
+      flag = False
+      fifo = []
 
-		height, width = image.shape
-		total = height * width
-		labels = np.full((height, width), self.INIT, np.int32)
+      height, width = image.shape
+      total = height * width
+      labels = np.full((height, width), self.INIT, np.int32)
 
-		reshaped_image = image.reshape(total)
-		# [y, x] pairs of pixel coordinates of the flattened image.
-		pixels = np.mgrid[0:height, 0:width].reshape(2, -1).T
-		# Coordinates of neighbour pixels for each pixel.
-		neighbours = np.array([self._get_neighbors(height, width, p) for p in pixels])
-		neighbours = neighbours.reshape(height, width)
+      reshaped_image = image.reshape(total)
+      # [y, x] pairs of pixel coordinates of the flattened image.
+      pixels = np.mgrid[0:height, 0:width].reshape(2, -1).T
+      # Coordinates of neighbour pixels for each pixel.
+      neighbours = np.array([self._get_neighbors(height, width, p) for p in pixels])
+      neighbours = neighbours.reshape(height, width)
 
-		indices = np.argsort(reshaped_image)
-		sorted_image = reshaped_image[indices]
-		sorted_pixels = pixels[indices]
+      indices = np.argsort(reshaped_image)
+      sorted_image = reshaped_image[indices]
+      sorted_pixels = pixels[indices]
 
-		image_min = sorted_image[0]
-		image_max = sorted_image[-1]
+      image_min = sorted_image[0]
+      image_max = sorted_image[-1]
 
-		levels = np.linspace(image_min, image_max, self.levels)
-		level_indices = []
-		current_level = 0
+      levels = np.linspace(image_min, image_max, self.levels)
+      level_indices = []
+      current_level = 0
 
-		for i in xrange(total):
-			if sorted_image[i] > levels[current_level]:
-				# Skip levels until the next highest one is reached.
-				while sorted_image[i] > levels[current_level]: current_level += 1
-				level_indices.append(i)
-		level_indices.append(total)
+      for i in xrange(total):
+         if sorted_image[i] > levels[current_level]:
+            # Skip levels until the next highest one is reached.
+            while sorted_image[i] > levels[current_level]: current_level += 1
+            level_indices.append(i)
+      level_indices.append(total)
 
-		start_index = 0
-		for stop_index in level_indices:
-			# Mask all pixels at the current level.
-			for p in sorted_pixels[start_index:stop_index]:
-				labels[p[0], p[1]] = self.MASK
-				# Initialize queue with neighbours of existing basins at the current level.
-				for q in neighbours[p[0], p[1]]:
-					# p == q is ignored here because labels[p] < WSHD
-					# if labels[q[0], q[1]] > 0 or labels[q[0], q[1]] == WSHD:
-					if labels[q[0], q[1]] >= self.WSHD:
-						labels[p[0], p[1]] = self.INQE
-						fifo.append(p)
-						break
+      start_index = 0
+      for stop_index in level_indices:
+         # Mask all pixels at the current level.
+         for p in sorted_pixels[start_index:stop_index]:
+            labels[p[0], p[1]] = self.MASK
+            # Initialize queue with neighbours of existing basins at the current level.
+            for q in neighbours[p[0], p[1]]:
+               # p == q is ignored here because labels[p] < WSHD
+               # if labels[q[0], q[1]] > 0 or labels[q[0], q[1]] == WSHD:
+               if labels[q[0], q[1]] >= self.WSHD:
+                  labels[p[0], p[1]] = self.INQE
+                  fifo.append(p)
+                  break
 
-			# Extend basins.
-			while fifo:
-				p = fifo.pop(0)
-				lab_p = labels[p[0], p[1]]
-				# Label p by inspecting neighbours.
-				for q in neighbours[p[0], p[1]]:
-					lab_q = labels[q[0], q[1]]
-					if lab_q > 0:
-						if lab_p == self.INQE or (lab_p >= self.WSHD and flag):
-							labels[p[0], p[1]] = lab_q
-						elif lab_p > 0 and lab_p != lab_q:
-							labels[p[0], p[1]] = self.WSHD
-							flag = False
-					elif lab_q == self.WSHD:
-						if lab_p == self.INQE:
-							labels[p[0], p[1]] = self.WSHD
-							flag = True
-					elif lab_q == self.MASK:
-						labels[q[0], q[1]] = self.INQE
-						fifo.append(q)
+         # Extend basins.
+         while fifo:
+            p = fifo.pop(0)
+            lab_p = labels[p[0], p[1]]
+            # Label p by inspecting neighbours.
+            for q in neighbours[p[0], p[1]]:
+               lab_q = labels[q[0], q[1]]
+               if lab_q > 0:
+                  if lab_p == self.INQE or (lab_p >= self.WSHD and flag):
+                     labels[p[0], p[1]] = lab_q
+                  elif lab_p > 0 and lab_p != lab_q:
+                     labels[p[0], p[1]] = self.WSHD
+                     flag = False
+               elif lab_q == self.WSHD:
+                  if lab_p == self.INQE:
+                     labels[p[0], p[1]] = self.WSHD
+                     flag = True
+               elif lab_q == self.MASK:
+                  labels[q[0], q[1]] = self.INQE
+                  fifo.append(q)
 
-			# Detect and process new minima at the current level.
-			for p in sorted_pixels[start_index:stop_index]:
-				if labels[p[0], p[1]] == self.MASK:
-					# p is inside a new minimum. Create a new label.
-					current_label += 1
-					fifo.append(p)
-					labels[p[0], p[1]] = current_label
-					while fifo:
-						q = fifo.pop(0)
-						for r in neighbours[q[0], q[1]]:
-							if labels[r[0], r[1]] == self.MASK:
-								fifo.append(r)
-								labels[r[0], r[1]] = current_label
+         # Detect and process new minima at the current level.
+         for p in sorted_pixels[start_index:stop_index]:
+            if labels[p[0], p[1]] == self.MASK:
+               # p is inside a new minimum. Create a new label.
+               current_label += 1
+               fifo.append(p)
+               labels[p[0], p[1]] = current_label
+               while fifo:
+                  q = fifo.pop(0)
+                  for r in neighbours[q[0], q[1]]:
+                     if labels[r[0], r[1]] == self.MASK:
+                        fifo.append(r)
+                        labels[r[0], r[1]] = current_label
 
-			start_index = stop_index
+         start_index = stop_index
 
-		return labels
+      return labels
 
 if __name__ == "__main__":
-	import sys
-	from PIL import Image
-	import matplotlib.pyplot as plt
-	from scipy.misc import imsave
+   import sys
+   from PIL import Image
+   import matplotlib.pyplot as plt
+   from scipy.misc import imsave
 
-	w = Watershed()
-	image = np.array(Image.open(sys.argv[1]))
-	labels = w.apply(image)
-	imsave('ws.png', labels)
-	# plt.imshow(labels, cmap='Paired', interpolation='nearest')
-	# plt.show()
+   w = Watershed()
+   image = np.array(Image.open(sys.argv[1]))
+   labels = w.apply(image)
+   imsave('ws.png', labels)
+   # plt.imshow(labels, cmap='Paired', interpolation='nearest')
+   # plt.show()
 
